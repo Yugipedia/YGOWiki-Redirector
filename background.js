d42d6702563c124a03d7edc754ebd4423f3eac4c
Corey Chambers <coreychambers123@gmail.com>
2019-07-04 21:42:23 -0400

    Initial commit

---
+++ background.js
@@ -0,0 +1,54 @@
+// Simple extension to redirect all requests to YGO Wikia to Yugipedia
+(function(){
+  'use strict';
+  let isPluginDisabled = false; // Variable storing whether or not the plugin is disabled.
+  let storage = window.storage || chrome.storage; // Make sure we have a storage API.
+
+  const WIKIA_REGEX = /^(ygo|yu-gi-oh|yugioh)\.(wikia|fandom)\.com$/i; // Used to match the domain of the old wikia/fandom to make sure we are redirecting the correct domain.
+
+  // Listen to before anytime the browser attempts to navigate to the old Wikia/Fandom sites.
+  chrome.webNavigation.onBeforeNavigate.addListener(
+    function(info) {
+      if(isPluginDisabled) { // Ignore all navigation requests when the extension is disabled.
+        console.log("YGOWikia intercepted, ignoring because plugin is disabled.");
+        return;
+      }
+
+      // Create a native URL object to more easily determine the path of the url and the domain.
+      const url = new URL(info.url);
+
+      const isWikia = WIKIA_REGEX.test(url.host); // Check to ensure the redirect is occurring on either the fandom/wikia domain.
+      // If domain isn't subdomain of wikia.com, ignore, also if it's not in the redirect filter
+      if (!isWikia) return;
+
+      // Generate new url
+      const host = 'yugipedia';
+      const redirectUrl = `https://${host}.com${url.pathname}`; // Create the redirect URL
+      console.log(`YGOWikia intercepted:  ${info.url}\nRedirecting to ${redirectUrl}`); 
+      // Redirect the old wikia request to new wiki
+      chrome.tabs.update(info.tabId,{url:redirectUrl});
+    });
+
+  function updateIcon(){
+    // Change the icon to match the state of the plugin.
+    chrome.browserAction.setIcon({ path: isPluginDisabled?"icon32_black.png":"icon32.png"  });
+  }
+
+  storage.local.get(['isDisabled'],(result)=>{
+      // Get the initial condition of whether or not the extension is disabled
+      isPluginDisabled= result ? result.isDisabled : false;
+      updateIcon(); // Update icon to match new state
+  });
+
+  // Anytime the state of the plugin changes, update the internal state of the background script.
+  storage.onChanged.addListener(
+      function(changes, areaName) {
+        // If isDisabled changed, update isPluginDisabled
+        if(changes["isDisabled"]!==undefined && changes["isDisabled"].newValue!=changes["isDisabled"].oldValue) {
+          console.log(`YGOWikia Redirector is now ${changes["isDisabled"].newValue?'disabled':'enabled'}`);
+          isPluginDisabled=changes["isDisabled"].newValue;
+          updateIcon();
+        }
+      }
+    );
+})();
